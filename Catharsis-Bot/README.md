d983b5c94fff2eb482f9d8260f1543da14450ade
Wawel <stiefan89@hotmail.com>
2019-08-12 20:20:51 +0200

    Add files via upload

---
+++ Catharsis-Bot/README.md
@@ -0,0 +1,3 @@
+# Catharsis-Club-Brazil
+
+javascript:(function(){$.getScript('https://raw.githack.com/DavidStefansky/Catharsis-Club-Brazil/master/extensionn.js');})();
---
+++ Catharsis-Bot/entrada2.js
@@ -0,0 +1,56 @@
+/*API.on(API.USER_JOIN, user => {
+	//if (user.id == "20574971")
+	//	API.sendChat("/Olê olê o cafajeste da lambada chegou bebê);
+
+	if (user.id == "22517314")
+		API.sendChat(`/me @${user.username} Princesinha 90's acaba de chegar`);
+	
+	if (user.id == "6507798")
+		API.sendChat(`/me @${user.username} Ponponpon escondam seus bombons, a formiguinha chegou`);
+	
+	//if (user.id == "4208767")
+	//	API.sendChat(`/me  @${user.username} Corram, AlCaponeRJ o gângster acaba de entrar`);
+
+	//if (user.id == "3998944")
+	//	API.sendChat(`/me  @${user.username} chegou. A festa acabou :merlinipolice: :merlinipopo:`);
+	
+	if (user.id == "")
+		API.sendChat(`/me @${user.username} `); 
+	
+	if (user.id == "")
+		API.sendChat(`/me @${user.username} `);
+	
+	if (user.id == "")
+		API.sendChat(`/me @${user.username} `);
+});*/
+API.on(API.USER_JOIN, user => { 			
+    if (user.id == "20574971")
+    API.sendChat(`/me @${user.username} Olê olê o cafajeste da lambada chegou bebê `);
+			
+    if (user.id == "22517314")
+    API.sendChat(`/me @${user.username} Princesinha 90's acaba de chegar`);
+			
+    if (user.id == "6507798")
+    API.sendChat(`/me @${user.username} Ponponpon escondam seus bombons, a formiguinha chegou`);
+			
+    if (user.id == "4208767")
+    API.sendChat(`/me @${user.username} Corram, AlCaponeRJ o gângster acaba de entrar`);
+			
+    if (user.id == "3998944")
+    API.sendChat(`/me @${user.username} chegou. A festa acabou :merlinipolice: :merlinipopo:`);
+			
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+			
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+			
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+			
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+			
+    if (user.id == "")
+    API.sendChat(`/me @${user.username} `);
+});
